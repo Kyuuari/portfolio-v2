ccceb15c3160011d61239ab5191fd0f7b5b1cfee
Chester Cari <chester_cari@outlook.com>
2023-03-03 00:52:56 -0500

    changes to copy

--- src/components/ProjectsSectionCS.tsx
+++ src/components/ProjectsSectionCS.tsx
@@ -10,8 +10,10 @@ const ProjectsSectionCS = () => {
           <div className="place-items-center">
             <h1 className="text-2xl font-bold">Coming Soon</h1>
             <p className="max-w-[60ch]">
-              Sorry, currently working on new projects and experimenting, In the
-              mean time please feel free to check out my previous projects from{" "}
+              Exciting new projects are currently in the works! While I'm
+              experimenting and pushing my creativity, my latest work is not
+              quite ready to be showcased. In the meantime, feel free to check
+              out my previous projects on my{" "}
               <a className="text-red-600" href="https://github.com/kyuuari">
                 Github
               </a>{" "}
@@ -19,6 +21,7 @@ const ProjectsSectionCS = () => {
               <a className="text-red-600" href="https://kyuuari.github.io/">
                 Portfolio-v1
               </a>
+              . Stay tuned for updates on what I'm working on next!
             </p>
           </div>
           {/* </div> */}
